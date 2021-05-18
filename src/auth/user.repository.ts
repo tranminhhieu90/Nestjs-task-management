d0d56ca3ded7a550a06e5b349a4c48f7594b3c7d
polsjapan <hieu.dev90@gmail.com>
2021-05-18 10:51:29 +0700

    hash and validate password when signin

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,5 @@
 import { AuthCredentialsDto } from './dto/auth_credentials.dto';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { EntityRepository, Repository } from "typeorm";
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
@@ -7,10 +8,11 @@ import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 export class UserRepository extends Repository<User>{
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
     try {
       await user.save()
     } catch (error) {
@@ -20,5 +22,19 @@ export class UserRepository extends Repository<User>{
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string>{
+    const {username, password} = authCredentialsDto;
+    const user = await this.findOne({username});
+
+    if(user && await user.validatePassword(password)){
+      return username
+    } else {
+      return null
+    }
+  }
+  private async hashPassword(password: string, salt: string): Promise<string>{
+    return bcrypt.hash(password,salt)
   }
 }
